7a9d302d53d32bc2879e1d8df2389656a3d2764f
atogz <webdevatogz@gmail.co>
2020-04-11 16:03:14 +0300

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+<div>header</div>
+</template>
+
+<script>
+  export default {
+    name: "Header.vue"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="about bg-gray-200">
     <h1>This is an about page</h1>
   </div>
 </template>
