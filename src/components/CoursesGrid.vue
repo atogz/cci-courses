fd7e9d2dc1d098bbe9bea92874025d9c4e2eccc3
atogz <webdevatogz@gmail.co>
2020-04-14 01:31:58 +0300

    transitions

--- src/components/CoursesGrid.vue
+++ src/components/CoursesGrid.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="w-full grid grid-cols-4 gap-5 mt-12">
-       <div class="rounded overflow-hidden shadow-lg pb-2" v-for="course in courses" :key="course.id">
+       <div class="rounded overflow-hidden shadow-lg pb-2 flex flex-col" v-for="course in courses" :key="course.id">
            <img class="w-full" :src="course.backgroundImage" alt="Sunset in the mountains">
            <div class="px-6 py-4">
                <div class="font-bold text-xl mb-2 text-center">{{ course.name }}</div>
@@ -8,8 +8,8 @@
                    {{ course.description }}
                </p>
            </div>
-           <div class="px-6 py-4 flex justify-center">
-               <div class="bg-accentColor px-4 py-2 text-white rounded cursor-pointer">Записаться</div>
+           <div class="px-6 py-4 flex justify-center mt-auto">
+               <div class="bg-accentColor px-4 py-2 text-white rounded cursor-pointer transition duration-300 ease-in-out hover:opacity-75">Записаться</div>
            </div>
        </div>
    </div>
