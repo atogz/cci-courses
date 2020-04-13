448f70c6c5d0285200f45a4ebd0591b67a79897f
atogz <webdevatogz@gmail.co>
2020-04-14 00:08:40 +0300

    hero на home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>homepage</div>
+        <div class="w-full p-6 flex flex-col">
+            <div class="w-full flex justify-center mt-2 flex-wrap">
+                <h2 class="text-2xl">
+                    Обучающие курсы для команды <a class="text-accentColor hover:underline" target="_blank" href="https://coral.club">Coral Club</a>. Актуальные темы. Регулярные занятия.
+                </h2>
+                <h3 class="text-xl text-mainColor-lighter">
+                    Иностранные языки, коммуникации, программирование, и многое другое...
+                </h3>
+
+            </div>
+
+        </div>
 </template>
 
 
