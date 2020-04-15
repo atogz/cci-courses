b4e31e045855f2478fd25ed41e7f02855a6801c7
atogz <webdevatogz@gmail.co>
2020-04-15 11:53:16 +0300

    expandable courses menu

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="w-full flex p-6 pt-6 pb-6 border-t border-gray-400">
         <div class="w-1/4 flex flex-col">
-            <h3 class="font-bold pb-4">Информация</h3>
+            <h3 class="font-bold pb-4 text-xl">Информация</h3>
             <router-link to="/about" class="hover:underline">О проекте</router-link>
             <a href="mailto:info@courses.coral.club" class="hover:underline mt-1">info@courses.coral.club</a>
         </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,16 @@
                 <router-link to="/">
                     <img src="../assets/img/logo.png">
                 </router-link>
-                <router-link class="ml-8 text-mainColor-lighter hover:text-accentColor" to="/courses">Курсы</router-link>
+                <div class="ml-8" @click="showCoursesMenu = true" v-on-clickaway="hideShowCoursesMenu">
+                    <span class="course-toggle text-mainColor-lighter hover:text-accentColor cursor-pointer">Выберите курс</span>
+                    <div class="whitespace-no-wrap flex flex-col absolute bg-white border border-gray-300 w-auto mt-2" v-if="showCoursesMenu">
+                        <div id="dropdown-arrow" class="border-t border-gray-300 border-l"></div>
+                        <router-link to="/courses" class="py-2 px-4 mt-2 transition duration-300 ease-in-out hover:bg-gray-300">Социальные сети</router-link>
+                        <router-link to="/courses" class="py-2 px-4 mt-2 transition duration-300 ease-in-out hover:bg-gray-300">Смартфоны</router-link>
+                        <router-link to="/courses" class="py-2 px-4 mt-2 transition duration-300 ease-in-out hover:bg-gray-300">Web-разработка</router-link>
+                        <router-link to="/courses" class="py-2 px-4 mt-2 mb-2 transition duration-300 ease-in-out hover:bg-gray-300">T-SQL</router-link>
+                    </div>
+                </div>
                 <router-link class="ml-6 text-mainColor-lighter hover:text-accentColor"  to="/about">О проекте</router-link>
                 <router-link class="ml-6 text-mainColor-lighter hover:text-accentColor"  to="/teaching">Есть чему научить?</router-link>
                 <router-link class="ml-6 text-mainColor-lighter hover:text-accentColor" to="/contacts">Контакты</router-link>
@@ -15,11 +24,43 @@
 </template>
 
 <script>
+  import { mixin as clickaway } from 'vue-clickaway';
   export default {
-    name: "Header.vue"
+    mixins: [ clickaway ],
+    name: "Header.vue",
+    data() {
+      return {
+        showCoursesMenu: false
+      }
+    },
+    methods: {
+      hideShowCoursesMenu() {
+        this.showCoursesMenu = false;
+      }
+    }
   }
 </script>
 
 <style scoped>
+    #dropdown-arrow {
+        height: 7px;
+        width: 7px;
+        transform: rotate(45deg);
+        position: absolute;
+        top: -1.8%;
+        left: 5%;
+        background-color: #fff;
+    }
 
+    .course-toggle:after {
+        content: "";
+        display: inline-block;
+        width: .32em;
+        height: .32em;
+        border-bottom: 1px solid;
+        border-left: 1px solid;
+        margin-left: .4em;
+        margin-bottom: .22em;
+        transform: rotate(-45deg);
+    }
 </style>
