c68e62b98de066d67ed73d0bce0724bbc6ca65be
atogz <webdevatogz@gmail.com>
2020-04-19 19:50:37 +0300

    api

--- api/routers/mail.php
+++ api/routers/mail.php
@@ -1,43 +1,56 @@
 <?php
-class mail extends Api
+
+
+class Mail extends Api
 {
-
-
 	public function POST()
 	{
-
 			$customerName = $this->data["customerName"];
 			$phoneNumber = $this->data["phoneNumber"];
 			$email = $this->data["email"];
 			$messageType = $this->data["messageType"];
 			$message = $this->data["message"];
 
-			$to = 'webdevatogz@gmail.com';
+			switch($messageType) {
+				case 'contactsPage':
+					$messageType = 'вопрос со страницы контактов';
+					break;
+				case 'courseSubscribe':
+					$messageType = 'запись на курс';
+					break;
+				case 'teaching':
+					$messageType = 'хочу стать преподавателем';
+					break;
+				default:
+					$messageType = 'тема не указана';
+			}
+
+			$to = 'a.ragulin@coral-club.com';
 			$from = 'no-reply@courses.coral.club';
 			$subject = 'Сообщение с сайта courses.coral.club';
 			$headers = "From: " . $from . "\r\n";
 			$headers .= "Reply-To: ". $from . "\r\n";
 			$headers .= "MIME-Version: 1.0\r\n";
 			$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+			$headers .= "cc: nadin_hi@mail.ru";
 
 		$message =
 			"
 			        <html>
 			        <head></head>
 			        <body>
-			            <h3>Новое сообщение с сайта courses.coral.club. Тема: <b>".$messageType."</b></h3>
-			            
+			            <h3>Новое сообщение с сайта courses.coral.club.<br/><br/> Тема: <b>".$messageType."</b></h3>
+
 			            <p>Имя: $customerName</p>
 			            <p>Телефон: $phoneNumber</p>
 			           	<p>E-mail: $email</p>
-			           	<br/>
-			            <h5 style='text-decoration: underline'>Сообщение</h5>    
+			            <h4 style='text-decoration: underline'>Сообщение</h4>
 			            <p>$message</p>
 			        </body>
 			        </html>";
 
 			if(mail($to, $subject, $message, $headers)) {
-				$data["message"] = "Спасибо! Ваша заявка отправлена. В ближайшее время наш мы с вами свяжемся.";
+				$data["message"] = "Спасибо! Ваша заявка отправлена. В ближайшее время мы с вами свяжемся.";
 			}
 			else {
 				$data["error"] = error_get_last();
--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,48 +1,72 @@
 <template>
-    <form class="w-full max-w-lg mx-auto mb-8">
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                    Имя
-                </label>
-                <input class="appearance-none block w-full text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Иван" v-model="customerName" :class="{'border border-red-500': errors.includes('name')}">
-                <p class="text-red-500 text-xs italic" v-if="errors.includes('name')">Пожалуйста, заполните это поле</p>
+    <main>
+        <form class="w-full max-w-lg mx-auto mb-8">
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                        Имя
+                    </label>
+                    <input class="appearance-none block w-full text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Иван" v-model="customerName" :class="{'border border-red-500': errors.includes('name')}">
+                    <p class="text-red-500 text-xs italic" v-if="errors.includes('name')">Пожалуйста, заполните это поле</p>
+                </div>
+                <div class="w-full md:w-1/2 px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-phone-number">
+                        Телефон
+                    </label>
+                    <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3  leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-phone-number" type="tel" placeholder="+7(495)111-11-11" v-model="customerPhoneNumber" :class="{'border border-red-500': errors.includes('phone')}">
+                    <p class="text-red-500 text-xs italic" v-if="errors.includes('phone')">Пожалуйста, заполните это поле</p>
+                    <p class="text-red-500 text-xs italic" v-if="errors.includes('invalidPhoneNumber')">Пожалуйста, введите корретный номер телефона</p>
+                </div>
             </div>
-            <div class="w-full md:w-1/2 px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-phone-number">
-                    Телефон
-                </label>
-                <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3  leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-phone-number" type="tel" placeholder="+7(495)111-11-11" v-model="customerPhoneNumber" :class="{'border border-red-500': errors.includes('phone')}">
-                <p class="text-red-500 text-xs italic" v-if="errors.includes('phone')">Пожалуйста, заполните это поле</p>
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
+                        E-mail
+                    </label>
+                    <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="example@mail.com" v-model="customerEmail" :class="{'border border-red-500': errors.includes('email')}">
+                    <p class="text-red-500 text-xs italic" v-if="errors.includes('email')">Пожалуйста, заполните это поле</p>
+                    <p class="text-red-500 text-xs italic" v-if="errors.includes('invalidEmail')">Пожалуйста, укажите корректный e-mail</p>
+                </div>
             </div>
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="message">
+                        Сообщение
+                    </label>
+                    <textarea class=" no-resize appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" v-model="customerMessage" :class="{'border border-red-500': errors.includes('message')}"></textarea>
+                    <p class="text-red-500 text-xs italic" v-if="errors.includes('message')">Пожалуйста, заполните это поле</p>
+                </div>
+            </div>
+            <div class="flex items-center justify-center color-accentColor" v-if="loading">
+                <div class="sk-circle">
+                    <div class="sk-circle1 sk-child"></div>
+                    <div class="sk-circle2 sk-child"></div>
+                    <div class="sk-circle3 sk-child"></div>
+                    <div class="sk-circle4 sk-child"></div>
+                    <div class="sk-circle5 sk-child"></div>
+                    <div class="sk-circle6 sk-child"></div>
+                    <div class="sk-circle7 sk-child"></div>
+                    <div class="sk-circle8 sk-child"></div>
+                    <div class="sk-circle9 sk-child"></div>
+                    <div class="sk-circle10 sk-child"></div>
+                    <div class="sk-circle11 sk-child"></div>
+                    <div class="sk-circle12 sk-child"></div>
+                </div>
+            </div>
+            <div class="flex items-center" v-else>
+                <div class="md:w-1/3">
+                    <button class="shadow bg-accentColor transition duration-300 ease-in-out hover:opacity-75 focus:shadow-outline focus:outline-none text-white py-2 px-4 rounded" type="button" @click="sendMail">
+                        Отправить
+                    </button>
+                </div>
+                <div class="md:w-2/3"></div>
+            </div>
+        </form>
+        <div class="w-full p-4" v-if="responseMessage.length">
+            {{ responseMessage }}
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
-                    E-mail
-                </label>
-                <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="example@mail.com" v-model="customerEmail" :class="{'border border-red-500': errors.includes('email')}">
-                <p class="text-red-500 text-xs italic" v-if="errors.includes('email')">Пожалуйста, заполните это поле</p>
-            </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="message">
-                    Сообщение
-                </label>
-                <textarea class=" no-resize appearance-none block w-full  text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" v-model="customerMessage" :class="{'border border-red-500': errors.includes('message')}"></textarea>
-                <p class="text-red-500 text-xs italic" v-if="errors.includes('message')">Пожалуйста, заполните это поле</p>
-            </div>
-        </div>
-        <div class="md:flex md:items-center">
-            <div class="md:w-1/3">
-                <button class="shadow bg-accentColor transition duration-300 ease-in-out hover:opacity-75 focus:shadow-outline focus:outline-none text-white py-2 px-4 rounded" type="button" @click="sendMail">
-                    Отправить
-                </button>
-            </div>
-            <div class="md:w-2/3"></div>
-        </div>
-    </form>
+    </main>
+
 </template>
 
 <script>
@@ -56,7 +80,9 @@
         customerPhoneNumber: "",
         customerEmail: "",
         customerMessage: "",
-        errors: []
+        errors: [],
+        loading: false,
+        responseMessage: ""
       }
     },
     methods: {
@@ -70,30 +96,40 @@
         }
         if(!this.customerEmail.length) {
           this.errors.push('email');
+        } else {
+          let regExpMail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          if(!regExpMail.test(this.customerEmail)) {
+            this.errors.push('invalidEmail');
+          }
         }
         if(!this.customerMessage.length) {
           this.errors.push('message');
         }
 
+
       },
       sendMail() {
         this.validateForm();
         if(!this.errors.length) {
-          return axios.post('/api/mail.php', {
+          this.loading = true;
+          return axios.post('/api/mail/', {
             customerName: this.customerName,
             phoneNumber: this.customerPhoneNumber,
             email: this.customerEmail,
-            messageType: this.messageType,
-            message: this.message
+            messageType: this.type,
+            message: this.customerMessage
           })
-            .then(function (response) {
-              console.log(response);
+            .then( (response) => {
+              this.responseMessage = response.data.message;
+              this.loading = false;
             })
-            .catch(function (error) {
+            .catch( (error) => {
               console.log(error);
-            });
+              this.responseMessage = error;
+              this.loading = false;
+            })
         }
-        }
+      }
 
     },
     mounted() {
